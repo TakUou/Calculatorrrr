00f88252445dba61bdb5ad91cc4367a91b165dc0
TakUou <Tokimeki56@mail.ru>
2023-10-12 00:37:12 +0300

    ffff

---
+++ Calculatorrr/Calculator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatorrr
+{
+    internal class Calculator
+    {
+
+        private int Costyl;
+        public void Plus()
+        {
+            Costyl = 1;
+            Console.Write("Введите первое число: ");
+            string a = Console.ReadLine();
+            Console.Write("Введите второе число: ");
+            string b = Console.ReadLine();
+            CheckNumbers(a, b);
+
+            var c = Convert.ToInt32(a) + Convert.ToInt32(b);
+            Console.WriteLine ("Результат сложения: " + c);
+
+
+        }
+
+        public void Minus()
+        {
+            Costyl = 2;
+            Console.Write("Введите первое число: ");
+            string a = Console.ReadLine();
+            Console.Write("Введите второе число: ");
+            string b = Console.ReadLine();
+            CheckNumbers(a, b);
+
+            var c = Convert.ToInt32(a) - Convert.ToInt32(b);
+            Console.WriteLine("Результат разности: " + c);
+        }
+
+        public void Multiplication()
+        {
+            Costyl = 3;
+            Console.Write("Введите первое число: ");
+            string a = Console.ReadLine();
+            Console.Write("Введите второе число: ");
+            string b = Console.ReadLine();
+            CheckNumbers(a, b);
+
+            var c = Convert.ToInt32(a) * Convert.ToInt32(b);
+            Console.WriteLine("Результат умножения: "+ c);
+        }
+
+        public void Division()
+        {
+            Costyl = 4;
+            Console.Write("Введите первое число: ");
+            string a = Console.ReadLine();
+            Console.Write("Введите второе число: ");
+            string b = Console.ReadLine();
+            CheckNumbers(a, b);
+
+            double c = Convert.ToDouble(a) / Convert.ToDouble(b);
+            Console.WriteLine("Результат деления: "+ c);
+        }
+
+        private void CheckNumbers(string a, string b)
+        {
+            foreach (char i in a) 
+            {
+                if (!Char.IsNumber(i))
+                {
+                    Console.WriteLine("Что-то не так. Попробуйте снова...");
+                    CostylMethod();
+
+                }
+            }
+
+            foreach (char i in b)
+            {
+                if (!Char.IsNumber(i))
+                {
+                    Console.WriteLine("Что-то не так. Попробуйте снова...");
+                    CostylMethod();
+                }
+            }
+        }
+        private void CostylMethod()
+        {
+            switch(Costyl)
+            {
+                case 1: Plus(); break;
+                case 2: Minus(); break;
+                case 3: Multiplication(); break;
+                case 4: Division(); break;
+            }
+        }
+    }
+}
---
+++ Calculatorrr/Program.cs
@@ -0,0 +1,44 @@
+﻿using Calculatorrr;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        Menu();
+
+
+        static void Menu()
+        {
+            Calculator calc = new Calculator();
+            int a = 0;
+            while (true)
+            {
+                Console.WriteLine("Выберите действие: \n 1. Сложение \n 2. Вычитание \n 3. Умножение \n 4. Деление \n 5. Выход \n");
+                try
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Для выбора операции нужно воодить только цифру от 1 до 5!");
+                    Menu();
+                }
+                if (a == 5) break;
+                else
+                {
+                    switch (a)
+                    {
+                        case 1: calc.Plus(); break;
+                        case 2: calc.Minus(); break;
+                        case 3: calc.Multiplication(); break;
+                        case 4: calc.Division(); break;
+                        default: Console.WriteLine("Неверная операция! Попробуйте снова..."); break;
+                    }
+                }
+
+            }
+        }
+    }
+}
